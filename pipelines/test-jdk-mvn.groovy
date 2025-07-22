1a1d7ef408f0b91c6ad5477650399bccf11fd46e
guyvinay <mrsinghvinay563@gmial.com>
2025-07-23 01:10:50 +0530

    ansiColor

--- pipelines/test-jdk-mvn.groovy
+++ pipelines/test-jdk-mvn.groovy
@@ -13,19 +13,11 @@ pipeline {
     stages {
         stage('Verify Tools') {
             steps {
-                script {
-                    echo "\u001B[1;34m============================\u001B[0m"
+                ansiColor('xterm') {
                     echo "\u001B[1;34m🔧 Stage: Verify Java and Maven\u001B[0m"
-                    echo "\u001B[1;34m============================\u001B[0m"
-                }
-                echo "👉 Checking Java Version..."
-                sh 'java -version'
-
-                echo "👉 Checking Maven Version..."
-                sh 'mvn -version'
-
-                script {
-                    echo "\u001B[1;32m✅ Tools verification completed successfully.\u001B[0m"
+                    sh 'java -version'
+                    sh 'mvn -version'
+                    echo "\u001B[1;32m✅ Tool check completed\u001B[0m"
                 }
             }
         }
